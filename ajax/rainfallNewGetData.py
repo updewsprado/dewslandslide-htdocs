5b5b08838e9c75e695ad05e7db58ec5dc4dd2848
ivy jean <diligivyjean@gmail.com>
2016-10-27 17:20:46 +0800

    -updated python file of rainfall

--- ajax/rainfallNewGetData.py
+++ ajax/rainfallNewGetData.py
@@ -16,36 +16,19 @@ def getDF():
     # set arbitrarily for now
 #    fdate = "2016-04-25"
 #    tdate = "2016-05-25"
-#    
-    # url = "http://weather.asti.dost.gov.ph/home/index.php/api/data/%s/from/%s/to/%s" % (rsite,fdate,tdate)
-    # r = requests.get(url)
-
-    # df = pd.DataFrame(r.json()["data"])
-    # df = df.set_index(['dateTimeRead'])
-    # df.index = pd.to_datetime(df.index)
-
     engine = create_engine('mysql+pymysql://updews:october50sites@127.0.0.1/senslopedb')
-
-    #Changed date difference is 1 day or 24 hours
     query = "select timestamp, rain from senslopedb.%s where timestamp between '%s' and  '%s'" % (rsite ,fdate,tdate)
-
     df = pd.io.sql.read_sql(query,engine)
     df.columns = ['ts','rain']
     df = df.set_index(['ts'])
-
     df = df["rain"].astype(float)
-    df = df.resample('15Min', how = "sum").fillna(0.00)
+    df = df.resample('15Min', how = "sum")
     dfs = pd.rolling_sum(df,96,min_periods=1)
     dfs1 = pd.rolling_sum(df,288,min_periods=1)
     dfs = dfs[dfs>=0]
     dfs1 = dfs1[dfs1>=0]
-    dfa = pd.DataFrame({"rval":df,"cumm":dfs,"hrs72":dfs1})
-    dfa = dfa.fillna(0)
-    # dfa = dfa[96:]
- 
-    
+    dfa = pd.DataFrame({"rval":df,"cumm":dfs,"hrs72":dfs1})    
     dfajson = dfa.reset_index().to_json(orient="records",date_format='iso')
-    #dfa = dfa.reset_index()
     dfajson = dfajson.replace("T"," ").replace("Z","").replace(".000","")
     print dfajson
     
--- ajax/rainfallNewGetDataARQ.py
+++ ajax/rainfallNewGetDataARQ.py
@@ -16,35 +16,19 @@ def getDF():
     # set arbitrarily for now
 #    fdate = "2015-04-05"
 #    tdate = "2016-03-29"
-    
-    # url = "http://weather.asti.dost.gov.ph/home/index.php/api/data/%s/from/%s/to/%s" % (rsite,fdate,tdate)
-    # r = requests.get(url)
-
-    # df = pd.DataFrame(r.json()["data"])
-    # df = df.set_index(['dateTimeRead'])
-    # df.index = pd.to_datetime(df.index)
-
     engine = create_engine('mysql+pymysql://updews:october50sites@127.0.0.1/senslopedb')
-
-    #Changed date difference is 1 day or 24 hours
     query = "select timestamp, r15m, r24h from senslopedb.%s where timestamp between '%s' and '%s'" % (rsite ,fdate ,tdate)
-
     df = pd.io.sql.read_sql(query,engine)
     df.columns = ['ts','r15m','r24h']
     df = df.set_index(['ts'])
-
     df = df["r15m"].astype(float)
-    df = df.resample('15Min', how = "sum").fillna(0.00)
+    df = df.resample('15Min', how = "sum")
     dfs = pd.rolling_sum(df,96,min_periods=1)
     dfs1 = pd.rolling_sum(df,288,min_periods=1)
     dfs = dfs[dfs>=0]
     dfs1 = dfs1[dfs1>=0]
     dfa = pd.DataFrame({"rval":df,"cumm":dfs,"hrs72":dfs1})
-   
-    dfa = dfa.fillna(0)
-    
     dfajson = dfa.reset_index().to_json(orient="records",date_format='iso')
-    #dfa = dfa.reset_index()
     dfajson = dfajson.replace("T"," ").replace("Z","").replace(".000","")
     print dfajson
     
