8d3e9a43ac218c66cd7497ead926b4a0ed39568f
ivy jean <diligivyjean@gmail.com>
2016-12-06 17:45:11 +0800

    -updated path

--- ajax/colposgen.py
+++ ajax/colposgen.py
@@ -4,7 +4,7 @@ import time
 from datetime import datetime
 import pandas as pd
 
-path = os.path.abspath(os.path.join(os.path.dirname(__file__), '../../../updews-pycodes/Analysis/'))
+path = os.path.abspath(os.path.join(os.path.dirname(__file__), '../../updews-pycodes/Analysis/'))
 if not path in sys.path:
    sys.path.insert(1, path)
 del path
