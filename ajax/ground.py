040780de88d95de77d4f5626c1d1e3798d24a773
ivy jean <diligivyjean@gmail.com>
2016-12-07 11:39:37 +0800

    -new path for ground alerts

--- ajax/ground.py
+++ ajax/ground.py
@@ -3,14 +3,18 @@ import sys
 import time
 from datetime import datetime
 import pandas as pd
+path = os.path.abspath(os.path.join(os.path.dirname(__file__), '../../updews-pycodes/Data Analysis/GroundAlert/'))
+if not path in sys.path:
+   sys.path.insert(1, path)
+del path
 import GroundDataAlertLibWithTrending as test
-import querySenslopeDb as qs
+
 
     
 def getDF():
 
-        site = sys.argv[1]
-        cid = sys.argv[2]
+        site = "nur"
+        cid = "CRACK 1"
         df= test.GroundDataTrendingPlotJSON(site, cid, datetime.now())
         df = pd.DataFrame(df,columns=['raw'])
         dfajson = df.reset_index().to_json(orient="records",date_format='iso')
