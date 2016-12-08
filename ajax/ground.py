8f5bc5288dabdb06d38e6d59f31956b832bff7c9
ivy jean <diligivyjean@gmail.com>
2016-12-08 10:54:47 +0800

    -updated path for ground alert

--- ajax/ground.py
+++ ajax/ground.py
@@ -3,7 +3,7 @@ import sys
 import time
 from datetime import datetime
 import pandas as pd
-path = os.path.abspath(os.path.join(os.path.dirname(__file__), '../../updews-pycodes/Data Analysis/GroundAlert/'))
+path = os.path.abspath(os.path.join(os.path.dirname(__file__), '../../updews-pycodes/Analysis/GroundAlert/'))
 if not path in sys.path:
    sys.path.insert(1, path)
 del path
@@ -13,8 +13,8 @@ import GroundDataAlertLibWithTrending as test
     
 def getDF():
 
-        site = "nur"
-        cid = "CRACK 1"
+        site = sys.argv[1]
+        cid = sys.argv[2]
         df= test.GroundDataTrendingPlotJSON(site, cid, datetime.now())
         df = pd.DataFrame(df,columns=['raw'])
         dfajson = df.reset_index().to_json(orient="records",date_format='iso')
