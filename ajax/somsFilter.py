2a0bf561c8e2fe149294caa0184a556e8c6a738a
ivy jean <diligivyjean@gmail.com>
2016-12-13 17:18:20 +0800

    -somsfilter

---
+++ ajax/somsFilter.py
@@ -0,0 +1,37 @@
+
+import os
+import sys
+import pandas as pd
+import numpy as np
+from datetime import timedelta as td
+from datetime import datetime as dt
+import sqlalchemy
+from sqlalchemy import create_engine
+import requests
+path = os.path.abspath(os.path.join(os.path.dirname(__file__), '../../updews-pycodes/Analysis/'))
+if not path in sys.path:
+   sys.path.insert(1, path)
+del path
+import SomsRangeFilter 
+import ConvertSomsRaw as CSR
+    
+site = sys.argv[1]
+fdate = sys.argv[2]
+tdate = sys.argv[3]
+nid = sys.argv[4]
+mode = sys.argv[5]
+#site = 'gaasb'
+#nid = 2
+#mode = 1
+#fdate = '2016-04-01'
+#tdate = '2016-04-05'
+if mode == 0:
+   df = CSR.getsomsrawdata(site + 'm',int(nid),fdate,tdate)
+else:
+   df = CSR.getsomscaldata(site + 'm',int(nid),fdate,tdate)
+
+df_filt = SomsRangeFilter.f_outlier(df,site,int(nid),int(mode)).first()
+dfajson = df_filt.reset_index().to_json(orient='records',date_format='iso')
+dfajson = dfajson.replace("T"," ").replace("Z","").replace(".000","")
+print dfajson
+#print  type(fdate)
