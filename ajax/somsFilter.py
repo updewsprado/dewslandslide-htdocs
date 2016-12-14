1e0e46dbe4b7f547618418b99b64e505e68ad052
ivy jean <diligivyjean@gmail.com>
2016-12-14 18:44:55 +0800

    -soms updated code

--- ajax/somsFilter.py
+++ ajax/somsFilter.py
@@ -16,22 +16,22 @@ import SomsRangeFilter
 import ConvertSomsRaw as CSR
     
 site = sys.argv[1]
-fdate = sys.argv[4]
-tdate = sys.argv[5]
-nid = sys.argv[2]
-mode = int(sys.argv[3])
-#site = 'gaasb'
-#nid = 2
-#mode = int('1')
-#fdate = '2016-04-01'
-#tdate = '2016-04-05'
+fdate = sys.argv[2]
+tdate = sys.argv[3]
+nid = sys.argv[4]
+mode = sys.argv[5]
+# site = 'gaasb'
+# nid = 2
+# mode = 1
+# fdate = '2016-04-01'
+# tdate ='2016-04-05'
 if mode == 0:
    df = CSR.getsomsrawdata(site,int(nid),fdate,tdate)
 else:
    df = CSR.getsomscaldata(site,int(nid),fdate,tdate)
 
-df_filt = SomsRangeFilter.f_outlier(df,site,int(nid),mode)
+df_filt = SomsRangeFilter.f_outlier(df,site,int(nid),int(mode))
 dfajson = df_filt.reset_index().to_json(orient='records',date_format='iso')
 dfajson = dfajson.replace("T"," ").replace("Z","").replace(".000","")
 print dfajson
-#print  type(fdate)
+# print  type(fdate)
