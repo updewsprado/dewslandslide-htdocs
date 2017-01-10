88c2bdedfaecd48d937ccf6367146807e4eb7545
ivy jean <diligivyjean@gmail.com>
2017-01-10 16:23:35 +0800

    - accel test

--- ajax/accelfilteredData.py
+++ ajax/accelfilteredData.py
@@ -9,16 +9,16 @@ import filterSensorData
 # import querySenslopeDb as qs
     
 def getDF():
-#        site = sys.argv[1]
-#        fdate = sys.argv[2]
-#        tdate = sys.argv[3]
-#        mid = sys.argv[4]
-#        nodeid = sys.argv[5]
-        site = 'agbsb'
-        fdate = '2015-04-10'
-        tdate = '2016-11-10'
-        mid = '33'
-        nodeid = '1'
+        site = sys.argv[1]
+        fdate = sys.argv[2]
+        tdate = sys.argv[3]
+        mid = sys.argv[4]
+        nodeid = sys.argv[5]
+#        site = 'agbsb'
+#        fdate = '2015-04-10'
+#        tdate = '2016-11-10'
+#        mid = '33'
+#        nodeid = '1'
         df= GetRawAccelData(siteid = site, fromTime = fdate, toTime = tdate,  maxnode = 40, msgid = int(mid), targetnode =  int(nodeid) , batt=1, returndb=True)
 #        df_filt = filterSensorData.applyFilters(df, orthof=True, rangef=True, outlierf=True)
         df_filt = df.set_index(['ts'])
