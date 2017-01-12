c5a3f5a95cee6886c9580be461cbb193d46653c1
ivy jean <diligivyjean@gmail.com>
2017-01-12 17:16:48 +0800

    -debbuging code for test only

--- ajax/accelfilteredData.py
+++ ajax/accelfilteredData.py
@@ -10,19 +10,22 @@ import filterSensorData
     
 def getDF():
     site = sys.argv[1]
-    # fdate = sys.argv[2]
-    # tdate = sys.argv[3]
+    fdate = sys.argv[2]
+    tdate = sys.argv[3]
     # mid = sys.argv[4]
-    # nodeid = sys.argv[5]
+    nodeid = sys.argv[5]
 #    site = 'agbsb'
-    fdate = '2015-04-10'
-    tdate = '2016-11-10'
-    mid = '33'
-    nodeid = '1'
-    df= GetRawAccelData(siteid = site, fromTime = fdate, toTime = tdate,  maxnode = 40, msgid = int(mid), targetnode =  int(nodeid) , batt=1, returndb=True)
+#    fdate = '2015-04-10'
+#    tdate = '2016-11-10'
+    mid = 33
+#    nodeid = '1'
+    df= GetRawAccelData(siteid = site, fromTime = fdate, toTime = tdate,  maxnode = 40, msgid = mid, targetnode =  int(nodeid) , batt=1, returndb=True)
     df_filt = filterSensorData.applyFilters(df, orthof=True, rangef=True, outlierf=True)
     df_filt = df_filt.set_index(['ts'])
     dfajson = df_filt.reset_index().to_json(orient='records',date_format='iso')
     dfajson = dfajson.replace("T"," ").replace("Z","").replace(".000","")
-    print dfajson   
+    print dfajson
+
+#        print df
+        
 getDF();
