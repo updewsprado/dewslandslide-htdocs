2399086949ad28160a64714a4d7f073b127fdf0e
ivy jean <diligivyjean@gmail.com>
2017-01-16 17:36:16 +0800

    -updated accelfilteredData.py for accel node

--- ajax/accelfilteredData.py
+++ ajax/accelfilteredData.py
@@ -6,6 +6,14 @@ if not path in sys.path:
 del path
 from querySenslopeDb import *
 import filterSensorData
+import pandas as pd
+import numpy as np
+from datetime import timedelta as td
+from datetime import datetime as dt
+import sqlalchemy
+from sqlalchemy import create_engine
+import sys
+import requests
 # import querySenslopeDb as qs
     
 def getDF():
@@ -17,15 +25,19 @@ def getDF():
 #    site = 'agbsb'
 #    fdate = '2015-04-10'
 #    tdate = '2016-11-10'
-#    mid = '33'
+#    mid = '32'
 #    nodeid = '1'
+    engine = create_engine('mysql+mysqldb://updews:october50sites@127.0.0.1/senslopedb')
+    query = "SELECT timestamp,id,msgid,xvalue,yvalue,zvalue,batt FROM senslopedb.%s where id = '%s'and msgid ='%s' and timestamp between '%s ' and '%s'" % (site,nodeid,mid,fdate,tdate)
+    df = pd.io.sql.read_sql(query,engine)
+#    df.columns = ['ts','rain']
+    df = df.set_index(['timestamp'])
     df= GetRawAccelData(siteid = site, fromTime = fdate, toTime = tdate,  maxnode = 40, msgid = int(mid), targetnode =  int(nodeid) , batt=1, returndb=True)
-    # df_filt = filterSensorData.applyFilters(df, orthof=True, rangef=True, outlierf=True)
+    df_filt = filterSensorData.applyFilters(df, orthof=True, rangef=True, outlierf=True)
     df_filt = df.set_index(['ts'])
     dfajson = df_filt.reset_index().to_json(orient='records',date_format='iso')
     dfajson = dfajson.replace("T"," ").replace("Z","").replace(".000","")
     print dfajson
-
-#        print df
+#    print df
         
 getDF();
