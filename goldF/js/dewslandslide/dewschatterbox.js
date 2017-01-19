84fd0e7748b75e9e132d1b43e8dd6d1e97ddb552
jgeliberte <gelibertejohn@gmail.com>
2017-01-19 15:43:00 +0800

    Initial Commit: Fixed UI

--- goldF/js/dewslandslide/dewschatterbox.js
+++ goldF/js/dewslandslide/dewschatterbox.js
@@ -611,9 +611,9 @@
 	//Connect the app to the Web Socket Server
 	function connectWS() {
 		console.log("trying to connect to web socket server");
-		var tempConn = new WebSocket('ws://www.dewslandslide.com:5050');
+		// var tempConn = new WebSocket('ws://www.dewslandslide.com:5050');
 		// var tempConn = new WebSocket('ws://54.166.60.233:5050'); // Other server
-		// var tempConn = new WebSocket('ws://localhost:5050'); // For local server
+		var tempConn = new WebSocket('ws://localhost:5050'); // For local server
 
 		tempConn.onopen = function(e) {
 			console.log("Connection established!");
