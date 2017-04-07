e4357cf3150b3bf89649e6c5401202fe7e7dfd06
John Geliberte <gelibertejohn@gmail.com>
2017-04-07 13:29:54 +0800

    Added extra filter for chatterbox smsrcv websocket call

--- js/dewslandslide/reports/gintags_report.js
+++ js/dewslandslide/reports/gintags_report.js
@@ -1,6 +1,28 @@
 $(document).ready(function() {
-    let gintag_table = $('#gintag_table').DataTable();
 	$.get( "../gintagshelper/getAllGintagDetails", function( data ) {
-		console.log(data);
+		var response = JSON.parse(data);
+		var dataset = [];
+		var dataraw= [];
+        response.forEach(function(raw) {
+        	for (var i = 0; i < Object.keys(raw).length; i) {
+        		// dataraw.push();
+        		console.log(raw.Object.keys(raw));
+        	}
+        });
+		var sample_data = [["HEY","HEY","HEY","HEY","HEY","HEY","HEY","HEY"]];
+		console.log(sample_data);
+		    $('#gintag_table').DataTable( {
+		        data: data,
+		        columns: [
+		            { title: "Gintag ID" },
+		            { title: "Tagger ID" },
+		            { title: "Table Element ID" },
+		            { title: "Table Used." },
+		            { title: "Timestamp date" },
+		            { title: "Remarks" },
+		            { title: "Tag name" },
+		            { title: "Tag Description" }
+		        ]
+	    });
 	});
 });
