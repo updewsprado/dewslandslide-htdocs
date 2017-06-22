5f1e75b036eec8e558c00322b01be0c8f6dad609
ivy jean dilig <diligivyjean@gmail.com>
2017-06-22 11:41:01 +0800

    -updated earth_quake.js

--- js/dewslandslide/data_analysis/earth_quake.js
+++ js/dewslandslide/data_analysis/earth_quake.js
@@ -7,33 +7,34 @@ $(document).ajaxStop(function () {
 
 $(document).ready(function(e) {
 	
-  $.get("../api/AllSiteDetails").done(function(data){
+	$.get("../api/AllSiteDetails").done(function(data){
 		var all_sites_details = JSON.parse(data);
 		var map_pins=[]
 		for (i = 0; i < all_sites_details.length; i++) {
 			map_pins.push([parseFloat(all_sites_details[i].lat) ,parseFloat(all_sites_details[i].lon),all_sites_details[i].name])
 		}
-		initMap(map_pins)
+		getAllData(map_pins)
 	})
-	function initMap(map_pins) {
-		var citymap = {
-		chicago: {
-			center: {lat: 41.878, lng: -87.629},
-			population: 2714856
-		},
-		newyork: {
-			center: {lat: 40.714, lng: -74.005},
-			population: 8405837
-		},
-		losangeles: {
-			center: {lat: 34.052, lng: -118.243},
-			population: 3857799
-		},
-		vancouver: {
-			center: {lat: 49.25, lng: -123.1},
-			population: 603502
-		}
-	};
+
+
+	function getAllData(map_pins) {
+		$.get("../api_testing/EarthquakeEvent/2017-06-01%2013:30:00/2017-06-02%2013:30:00").done(function(data){
+			var all_earthquake_details = JSON.parse(data);
+			initMap(map_pins,all_earthquake_details)
+		})
+
+	}
+
+	function initMap(map_pins,data) {
+
+
+		// var citymap = {
+		// 	chicago: {
+		// 		center: {lat: 11.310000, lng: 122.910000},
+		// 		population: 14.195*1000
+		// 	},
+
+		// };
 		var map = new google.maps.Map(document.getElementById('maps'), {
 			zoom: 6,
 			center: new google.maps.LatLng(11.8978,121.9094),
@@ -47,18 +48,20 @@ $(document).ready(function(e) {
 			});
 		}
 
-		for (var city in citymap) {
-          var cityCircle = new google.maps.Circle({
-            strokeColor: '#FF0000',
-            strokeOpacity: 0.8,
-            strokeWeight: 2,
-            fillColor: '#FF0000',
-            fillOpacity: 0.35,
-            map: map,
-            center: citymap[city].center,
-            radius: Math.sqrt(citymap[city].population) * 100
-        });
-      }
+
+		for (var i = 0; i < data.length; i++) {
+			var cityCircle = new google.maps.Circle({
+				strokeColor: '#FF0000',
+				strokeOpacity: 0.8,
+				strokeWeight: 2,
+				fillColor: '#FF0000',
+				fillOpacity: 0.35,
+				map: map,
+				center: new google.maps.LatLng(data[i].latitude,data[i].longitude),
+				radius: data[i].critical_distance * 1000
+			});
+		}
+		
 	}
 });
 
