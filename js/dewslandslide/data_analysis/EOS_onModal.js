e4bcd752617fdf8c1aab2d48a3b722e7c56cbb37
ivy jean dilig <diligivyjean@gmail.com>
2017-07-19 11:10:32 +0800

    testing

--- js/dewslandslide/data_analysis/EOS_onModal.js
+++ js/dewslandslide/data_analysis/EOS_onModal.js
@@ -375,6 +375,7 @@ function getRainSenslope(site,dataSubmit,max_rain,id,distance) {
 								chartProcessRain2(series_data2,id,'Senslope',site,max_rain,negative,dataTableSubmit,distance);
 							}else{
 								chartProcessRain2(undefined,id,'Senslope',site,max_rain,negative,dataTableSubmit,distance);
+								chartProcessRain(undefined,id,'Senslope',site,max_rain,dataTableSubmit,distance,max_value);
 							}
 							
 						}, 1000);
@@ -468,6 +469,7 @@ function getRainArq(site,dataSubmit,max_rain,id,distance) {
 								chartProcessRain2(series_data2,id,'ARQ',site,max_rain,negative,dataTableSubmit,distance );
 							}else{
 								chartProcessRain2(undefined,id,'ARQ',site,max_rain,negative,dataTableSubmit,distance);
+								chartProcessRain(undefined,id,'ARQ',site,max_rain,dataTableSubmit,distance,max_value );
 							}
 						}, 1000);
 					}else{
@@ -557,11 +559,11 @@ function getRainNoah(site,dataSubmit,max_rain,id,distance) {
 						}
 						setTimeout(function(){
 							chartProcessRain(series_data,id,'Noah',site,max_rain,dataTableSubmit,distance,max_value );
-							console.log(all_raindata[2].length)
 							if(all_raindata[2].length != 0){
 								chartProcessRain2(series_data2,id,'Noah',site,max_rain,negative,dataTableSubmit,distance );
 							}else{
 								chartProcessRain2(undefined,id,'Noah',site,max_rain,negative,dataTableSubmit,distance);
+								chartProcessRain(undefined,id,'Noah',site,max_rain,dataTableSubmit,distance,max_value );
 							}
 							
 						}, 1000);
@@ -572,7 +574,6 @@ function getRainNoah(site,dataSubmit,max_rain,id,distance) {
 							tdate : dataSubmit.tdate,
 							current_site : dataSubmit.site
 						}
-						console.log(series_data2)
 						chartProcessRain(series_data,id,'Noah',site,max_rain,dataTableSubmit,distance,max_value );
 						chartProcessRain2(series_data2,id,'Noah',site,max_rain,negative,dataTableSubmit,distance );
 					}
