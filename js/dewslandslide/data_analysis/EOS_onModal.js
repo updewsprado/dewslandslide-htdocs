cfff858ac999881a05e3a5afdb4544d1a1cffecd
ivy jean dilig <diligivyjean@gmail.com>
2017-07-19 11:02:36 +0800

    testing

--- js/dewslandslide/data_analysis/EOS_onModal.js
+++ js/dewslandslide/data_analysis/EOS_onModal.js
@@ -557,6 +557,7 @@ function getRainNoah(site,dataSubmit,max_rain,id,distance) {
 						}
 						setTimeout(function(){
 							chartProcessRain(series_data,id,'Noah',site,max_rain,dataTableSubmit,distance,max_value );
+							console.log(all_raindata[2].length)
 							if(all_raindata[2].length != undefined){
 								chartProcessRain2(series_data2,id,'Noah',site,max_rain,negative,dataTableSubmit,distance );
 							}else{
@@ -571,6 +572,7 @@ function getRainNoah(site,dataSubmit,max_rain,id,distance) {
 							tdate : dataSubmit.tdate,
 							current_site : dataSubmit.site
 						}
+						console.log(series_data2)
 						chartProcessRain(series_data,id,'Noah',site,max_rain,dataTableSubmit,distance,max_value );
 						chartProcessRain2(series_data2,id,'Noah',site,max_rain,negative,dataTableSubmit,distance );
 					}
