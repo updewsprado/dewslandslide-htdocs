67d7fba34a2add9020fa844d2bc23b0ba82464e5
ivy jean dilig <diligivyjean@gmail.com>
2017-08-09 13:47:46 +0800

    -testing

--- js/dewslandslide/data_analysis/EOS_onModal.js
+++ js/dewslandslide/data_analysis/EOS_onModal.js
@@ -1048,6 +1048,7 @@ function chartProcessSurficial(id,data_series,name,dataTableSubmit){
 }
 
 function allSensorPosition(site,fdate,tdate) {
+	console.log("/api/SensorAllAnalysisData/"+site+"/"+fdate+"/"+tdate)
 	$.ajax({url: "/api/SensorAllAnalysisData/"+site+"/"+fdate+"/"+tdate,
 		dataType: "json",
 		success: function(result){
