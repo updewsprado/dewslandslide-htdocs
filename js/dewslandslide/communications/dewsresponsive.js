9bd39ddd949046a38a26e81205520cadf55dead5
jgeliberte <gelibertejohn@gmail.com>
2017-08-31 16:57:27 +0800

    Initial commit for group message tabs

--- js/dewslandslide/communications/dewsresponsive.js
+++ js/dewslandslide/communications/dewsresponsive.js
@@ -13,7 +13,8 @@ $(document).ready(function() {
 		$('#quick-inbox-display').css({"height": division_height-158});
 		$('#quick-inbox-unknown-display').css({"height": division_height-158});
 		$('#quick-event-inbox-display').css({"height": division_height-158});
-		$('#quick-release-display').css({"height": division_height-219});
+		$('#quick-release-display').css({"height": division_height-262});
+		$('#group-message-display').css({"height": division_height-272});
 		$('.chat-message').css({"height": division_height-308});
 	}
 });
