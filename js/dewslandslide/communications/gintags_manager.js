e9467cd4e98d7634374962dd986d2328dc3cc2d2
John Geliberte <gelibertejohn@gmail.com>
2017-09-13 16:03:48 +0800

    Display gintag to narrative table and CRUD function

--- js/dewslandslide/communications/gintags_manager.js
+++ js/dewslandslide/communications/gintags_manager.js
@@ -1,13 +1,46 @@
 $(document).ready(function(e){
-    gintags_table = $('#gintags-datatable').DataTable( {
-        "processing": true,
-        "serverSide": false,
-        columns: [
-            {title:"TAG"},
-            {title:"DESCRIPTION"},
-            {title:"NARRATIVE INPUT"}
-        ]
+
+	getGintags(); // Initialize gintag autocomplete
+    
+	initGintagsTable();
+
+    $('#btn-confirm').on('click',function(){
+    	if ($('#gintag-ipt').val().trim() != "" && $('#gintag-description-ipt').val().trim() != "" &&
+    		$('#narrative-ipt').val().trim() != "") {
+
+    		if ($('#gintag-ipt').val().charAt(0) != "#") {
+    			$('#gintag-ipt').val("#"+$('#gintag-ipt').val());
+    		}
+
+    		var data = {
+	    		'tag': $('#gintag-ipt').val(),
+	    		'tag_description': $('#gintag-description-ipt').val(),
+	    		'narrative_input': $('#narrative-ipt').val()
+	    	}
+
+			$.post( "../gintags_manager/insertGintagNarratives/", {gintags: data})
+			.done(function(response) {
+				if (response == "true") {
+					$.notify("Tag successfully added.", {
+		    			position: "top center",
+		    			className: "success"
+
+		    		});
+				} else {
+		    		$.notify("Error occured, duplicate entry.", {
+		    			position: "top center",
+		    			className: "error"
+		    		});
+				}
+			});
+    	} else {
+    		$.notify("All fields are required.", {
+    			position: "top center",
+    			className:"error"
+    		});
+    	}
     });
+
 });
 
 function insertGintags() {
@@ -15,7 +48,15 @@ function insertGintags() {
 }
 
 function getGintags() {
-
+	$.get( "../gintags_manager/getTagsForAutocomplete", function( data ) {
+		var tag_list = JSON.parse(data);
+		var input = document.getElementById("gintag-ipt");
+		new Awesomplete(input, {
+			list: tag_list,
+			minChars: 3,
+			maxItems: 10
+		});
+	});
 }
 
 function updateGintags() {
@@ -24,4 +65,19 @@ function updateGintags() {
 
 function deleteToNarraitves() {
 	
+}
+
+function initGintagsTable() {
+    var gintags_table = $('#gintags-datatable').DataTable( {
+        "processing": true,
+        "serverSide": false,
+        "ajax": '../gintags_manager/getGintagTable',
+        columns: [
+        	{"data" : "id", title:"ID"},
+            {"data" : "tag_name", title:"TAG"},
+            {"data" : "description", title:"DESCRIPTION"},
+            {"data" : "narrative_input", title:"NARRATIVE INPUT"},
+            {"data" : "functions", title: "*"}
+        ]
+    });
 }
