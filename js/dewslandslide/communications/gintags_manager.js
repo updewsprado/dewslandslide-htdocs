b26e74fad420fce5d3a961c7a193df43047b63fd
John Geliberte <gelibertejohn@gmail.com>
2017-09-18 14:50:30 +0800

    Added validations

--- js/dewslandslide/communications/gintags_manager.js
+++ js/dewslandslide/communications/gintags_manager.js
@@ -2,6 +2,7 @@ $(document).ready(function(e){
 
 	var initTableContent = [];
 	var temp = "";
+	var disable_flag;
 
 	getGintags(); // Initialize gintag autocomplete
 	$.get('../gintags_manager/getGintagTable', function( data ) {
@@ -11,49 +12,100 @@ $(document).ready(function(e){
 	initGintagsTable();
 
     $('#btn-confirm').on('click',function(){
-    	if ($('#gintag-ipt').val().trim() != "" && $('#gintag-description-ipt').val().trim() != "" &&
-    		$('#narrative-ipt').val().trim() != "") {
 
-    		if ($('#gintag-ipt').val().charAt(0) != "#") {
-    			$('#gintag-ipt').val("#"+$('#gintag-ipt').val());
+		if ($('#gintag-ipt').val().charAt(0) != "#") {
+			$('#gintag-ipt').val("#"+$('#gintag-ipt').val());
+		}
+
+    	if ($('#btn-confirm').text() == "UPDATE") {
+    		if ($('#gintag-ipt').val().trim() != "" || $('#gintag-description-ipt').val().trim() != "" ||
+	    		$('#narrative-ipt').val().trim() != "" || $('#tag-id').val().trim() != "") {
+
+    			var data = {
+	    			'tag_id': $('#tag-id').val().trim(),
+	    			'tag': $('#gintag-ipt').val().trim(),
+	    			'tag_description': $('#gintag-description-ipt').val().trim(),
+	    			'narrative_input': $('#narrative-ipt').val().trim()
+	    		}
+
+	    		$.post( "../gintags_manager/updateGintagNarrative/", {gintags: data})
+				.done(function(response) {
+
+					console.log(response);
+					
+					if (response == "true") {
+						$.notify("Tag successfully updated.", {
+			    			position: "top center",
+			    			className: "success"
+
+			    		});
+
+			    		$.get('../gintags_manager/getGintagTable', function( data ) {
+							initTableContent = JSON.parse(data);
+						});
+
+						clearFields();
+
+			    		reloadGintagsTable();
+					} else {
+			    		$.notify("Error occured, duplicate entry.", {
+			    			position: "top center",
+			    			className: "error"
+			    		});
+					}
+				});
+
+    		} else {
+	    		$.notify("All fields are required.", {
+	    			position: "top center",
+	    			className:"error"
+	    		});
     		}
-
-    		var data = {
-	    		'tag': $('#gintag-ipt').val(),
-	    		'tag_description': $('#gintag-description-ipt').val(),
-	    		'narrative_input': $('#narrative-ipt').val()
-	    	}
-
-			$.post( "../gintags_manager/insertGintagNarratives/", {gintags: data})
-			.done(function(response) {
-				if (response == "true") {
-					$.notify("Tag successfully added.", {
-		    			position: "top center",
-		    			className: "success"
-
-		    		});
-
-		    		$.get('../gintags_manager/getGintagTable', function( data ) {
-						initTableContent = JSON.parse(data);
-					});
-
-					clearFields();
-
-		    		reloadGintagsTable();
-				} else {
-		    		$.notify("Error occured, duplicate entry.", {
-		    			position: "top center",
-		    			className: "error"
-		    		});
-				}
-			});
     	} else {
-    		$.notify("All fields are required.", {
-    			position: "top center",
-    			className:"error"
-    		});
+	    	if ($('#gintag-ipt').val().trim() != "" || $('#gintag-description-ipt').val().trim() != "" ||
+	    		$('#narrative-ipt').val().trim() != "") {
+
+	    		var data = {
+		    		'tag': $('#gintag-ipt').val().trim(),
+		    		'tag_description': $('#gintag-description-ipt').val().trim(),
+		    		'narrative_input': $('#narrative-ipt').val().trim()
+		    	}
+
+				$.post( "../gintags_manager/insertGintagNarratives/", {gintags: data})
+				.done(function(response) {
+					if (response == "true") {
+						$.notify("Tag successfully added.", {
+			    			position: "top center",
+			    			className: "success"
+
+			    		});
+
+			    		$.get('../gintags_manager/getGintagTable', function( data ) {
+							initTableContent = JSON.parse(data);
+						});
+
+						clearFields();
+
+			    		reloadGintagsTable();
+					} else {
+			    		$.notify("Error occured, duplicate entry.", {
+			    			position: "top center",
+			    			className: "error"
+			    		});
+					}
+				});
+	    	} else {
+	    		$.notify("All fields are required.", {
+	    			position: "top center",
+	    			className:"error"
+	    		});
+	    	}
     	}
     });
+
+	$('#btn-reset').click(function(){
+		clearFields();
+	});  
 
     $('#gintags-datatable tbody').on('click','tr:has(td) .delete',function(){
     	var table = $('#template_table').DataTable();
@@ -68,6 +120,8 @@ $(document).ready(function(e){
 
 		temp = data.tag_name;
 
+		console.log(data);
+		$('#tag-id').val(data.id);
 		$('#gintag-ipt').val(data.tag_name);
 		$('#gintag-description-ipt').val(data.description);
 		$('#narrative-ipt').val(data.narrative_input);
@@ -82,24 +136,32 @@ $(document).ready(function(e){
     	if (temp == "") {
 	    	for (var counter = 0; counter < initTableContent.data.length; counter++) {
 	    		if (initTableContent.data[counter].tag_name.toLowerCase() == $(this).val().toLowerCase()) {
-		    		$.notify("Tag exists. Edit it using the 'PEN' icon", {
+		    		$.notify("Tag exist, edit it using the 'PEN' icon", {
 		    			position: "top center",
 		    			className: "info",
 		    			autoHideDelay: 5000,
 		    		});
 		    		clearFields();
+		    		$('#btn-confirm').prop('disabled',true);
 		    		$('#gintag-ipt').focus();
+		    		break;
+	    		} else {
+	    			$('#btn-confirm').prop('disabled',false);
 	    		}
 	    	}
     	} else {
 			for (var counter = 0; counter < initTableContent.data.length; counter++) {
 	    		if (initTableContent.data[counter].tag_name.toLowerCase() == $(this).val().toLowerCase() && temp.toLowerCase() != $(this).val().toLowerCase()) {
-		    		$.notify("Duplicate tag.", {
+		    		$.notify("Tag exist, edit it using the 'PEN' icon", {
 		    			position: "top center",
 		    			className: "error",
 		    			autoHideDelay: 5000,
 		    		});
+		    		$('#btn-confirm').prop('disabled',true);
 		    		$('#gintag-ipt').focus();
+		    		break;
+	    		} else {
+	    			$('#btn-confirm').prop('disabled',false);
 	    		}
 	    	}
     	}
@@ -138,6 +200,7 @@ function initGintagsTable() {
             {"data" : "tag_name", title:"TAG"},
             {"data" : "description", title:"DESCRIPTION"},
             {"data" : "narrative_input", title:"NARRATIVE INPUT"},
+            {"data" : "last_update", title: "LAST UPDATE"},
             {"data" : "functions", title: "*"}
         ]
     });
@@ -156,6 +219,7 @@ function reloadGintagsTable() {
             {"data" : "tag_name", title:"TAG"},
             {"data" : "description", title:"DESCRIPTION"},
             {"data" : "narrative_input", title:"NARRATIVE INPUT"},
+            {"data" : "last_update", title: "LAST UPDATE"},
             {"data" : "functions", title: "*"}
         ]
     });
@@ -165,4 +229,9 @@ function clearFields() {
 	$('#gintag-ipt').val('');
 	$('#narrative-ipt').val('');
 	$('#gintag-description-ipt').val('');
+	$('#tag-id').val();
+	$('#btn-confirm').text('Confirm');
+	$('#btn-reset').text('Reset');
+	$("#btn-confirm").attr('class', 'btn btn-info');
+	$("#btn-reset").attr('class', 'btn btn-warning');
 }
