beaa24902809ba364815f20ec80e089368d07380
John Louie Nepomuceno <jlouienepomuceno@gmail.com>
2018-04-11 13:53:07 +0800

    Added two working test cases. Could not proceed on others due to lack of idea on the usage of the functions. Will need to refer to John.

--- js/dewslandslide/test/chatterbox_beta/dewschatterbox_helper_test.js
+++ js/dewslandslide/test/chatterbox_beta/dewschatterbox_helper_test.js
@@ -4,14 +4,6 @@ function appendInput (type, selector, name, value) {
 
 }
 
-describe("setTargetTime", () => {
-    it("should be equal to ...", () => {
-        const trigger = setTargetTime(1,45);
-        console.log(trigger);
-        expect(trigger).to.equal("Fri Apr 06 2018 01:45:00 GMT+0800 (+08)");
-    });
-});
-
 describe("arraysEqual", () => {
     it("should be equal to false", () => {
         const trigger = arraysEqual([1,2,3],[1,2,3]);
@@ -21,9 +13,33 @@ describe("arraysEqual", () => {
 });
 
 describe("trimmedContactNum", () => {
-    it("should be equal to false", () => {
-        const trigger = trimmedContactNum("09178141909");
+    it("should return an array with value \"9121231234\" at index 0", () => {
+        const trigger = trimmedContactNum("09121231234");
         console.log(trigger);
-        expect(trigger).to.equal(["9178141909"]);
+        expect(trigger[0]).to.equal("9121231234");
+    });
+});
+
+describe("normalizedContactNum", () => {
+    it("should return a String value 639121231234", () => {
+        const trigger = normalizedContactNum("09121231234");
+        console.log(trigger);
+        expect(trigger).to.equal("639121231234");
+    });
+});
+
+describe("loadSearchedMessage", () => {   // ON HOLD. ASK JOHN ON WHETHER THIS IS AN ACTIVE FUNCTION OR UNUSED FUNCTION
+    it("should return ...", () => {
+        const trigger = loadSearchedMessage("po");
+        console.log(trigger);
+        expect(trigger).to.equal();
+    });
+});
+
+describe("updateGlobalMessage", () => {
+    it("should ...", () => {
+        const trigger = updateGlobalMessage("");
+        console.log(trigger);
+        expect(trigger).to.equal("639121231234");
     });
 });
