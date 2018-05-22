f8aa66645e9d2657d3e049de59935d06df3d6b50
jhndvdgvr <david063095@gmail.com>
2018-05-22 13:22:30 +0800

    added mocha test for site analysis download chart

---
+++ js/dewslandslide/test/download_site_charts_test.js
@@ -0,0 +1,99 @@
+const { expect } = chai;
+
+
+// createRainFallSVG
+// 	- has appended 8 svg tag
+describe("createRainFallSVG", () => {
+	it("should append 8 rain sources svg chart", () => {
+    // const chart_plots = null;
+    // const SITE_LEVEL_CONTAINER = ["agbtaw-instantaneous", "agbtaw-cumulative", "557-instantaneous", "557-cumulative", "1255-instantaneous", "1255-cumulative", "1258-instantaneous", "1258-cumulative" ];
+    // createSVG("rainfall");
+	});
+});
+// createSurficialSVG
+// 	- has appended surficial svg
+
+describe("createSurficialSVG", () => {
+  it("should append surficial svg chart", () => {
+
+	});
+});
+
+// createColumnSummarySVG
+// 	- has appended node health, data presence and comm health svg
+
+describe("createSurficialSVG", () => {
+  it("should append node health svg chart", () => {
+
+	});
+
+  it("should append data presence svg chart", () => {
+
+	});
+
+  it("should append communication health svg chart", () => {
+
+	});
+});
+
+// createSubsurfaceSVG
+// 	- has appended subsurface charts (downslope and across slope)
+
+describe("createSubsurfaceSVG", () => {
+  it("should append column-position-downslope svg chart", () => {
+
+	});
+
+  it("should append column-position-across_slope svg chart", () => {
+
+	});
+
+  it("should append subsurface-displacement-downslope svg chart", () => {
+
+	});
+
+  it("should append subsurface-displacement-across_slope svg chart", () => {
+
+  });
+
+  it("should append velocity-alerts-downslope svg chart", () => {
+
+  });
+
+  it("should append velocity-alerts-across_slope svg chart", () => {
+
+  });
+});
+
+// createNodeSVG
+// 	- has appended x, y, z-accelerometer and battery chart
+describe("createNodeSVG", () => {
+  it("should append x-accelerometer svg chart", () => {
+
+	});
+
+  it("should append y-accelerometer svg chart", () => {
+
+	});
+
+  it("should append z-accelerometer svg chart", () => {
+
+	});
+
+  it("should append battery svg chart", () => {
+
+	});
+});
+
+// returnYaxisValue
+// 	- should return 400 or 800
+
+describe("returnYaxisValue", () => {
+  it("should return 400", () => {
+
+	});
+
+  it("should return 800", () => {
+
+	});
+});
